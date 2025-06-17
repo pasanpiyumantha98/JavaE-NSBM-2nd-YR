2d0a016f0a61846cbcb244b5ecfb38c99196fb6d
pasanpiyumantha98 <139437240+pasanpiyumantha98@users.noreply.github.com>
2025-06-17 21:34:39 +0530

    e3

--- e2.java
+++ e2.java
@@ -3,16 +3,30 @@ class Demo2{
 
     public static void main(String args[])
     {
+        String stat;
 
-        name("Pasan");
+        stat = club("Pasan", 27);
+
+        System.out.println(stat);
 
     }
 
 
-    public static void name(String name)
+    public static String club(String name, int age)
     {
+        boolean stat = false;
 
-        System.out.println("Your name is" + name);
+        if(age>21)
+          stat =true;
+
+        if(stat==true)
+        {
+            return("Mr." + name + " Welcome to the club");
+        } else
+        {
+             return("Mr." + name + " You cannot enter!");
+        }
+        
     }
 
 
