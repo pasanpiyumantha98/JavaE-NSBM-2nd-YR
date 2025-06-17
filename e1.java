562ecfca176be20a0fe1abca4c3c46ade65e4631
pasanpiyumantha98 <139437240+pasanpiyumantha98@users.noreply.github.com>
2025-06-17 21:14:19 +0530

    n1

--- e1.java
+++ e1.java
@@ -1,9 +1,16 @@
-class demo {
+class Demo {
 
 
     public static void main (String args[])
     {
-        System.out.println("My Name is Pasan")
+        int age =27;
+
+        double num1 =3433.32;
+
+        String name ="Pasan Piyumantha";
+
+
+        System.out.println("My name is" + name + " and my age is " + age);
     }
 
 }
