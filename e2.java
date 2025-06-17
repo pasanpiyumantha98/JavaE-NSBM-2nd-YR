88f10a95b398d0087d2aa66139fe2fcf391e9360
pasanpiyumantha98 <139437240+pasanpiyumantha98@users.noreply.github.com>
2025-06-17 21:24:36 +0530

    e2

---
+++ e2.java
@@ -0,0 +1,20 @@
+class Demo2{
+
+
+    public static void main(String args[])
+    {
+
+        name("Pasan");
+
+    }
+
+
+    public static void name(String name)
+    {
+
+        System.out.println("Your name is" + name);
+    }
+
+
+
+}
