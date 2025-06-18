d8beed7b8ab722aadb4592b147e95ed2de2eb045
pasanpiyumantha98 <139437240+pasanpiyumantha98@users.noreply.github.com>
2025-06-18 21:14:06 +0530

    18june

---
+++ objects.java
@@ -0,0 +1,30 @@
+class train{
+
+    int capacity;
+    String name;
+
+    public void trainname()
+    {
+        System.out.println("The train name is " + name);
+    }
+
+}
+
+
+
+
+class demo4{
+
+
+public static void main(String args[]){
+
+train t1 = new train();
+
+t1.name="Sagarika";
+
+t1.trainname();
+
+
+}
+
+}
