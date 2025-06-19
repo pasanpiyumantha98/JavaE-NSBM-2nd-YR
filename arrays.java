e6d250548b082fc73fc422e1f8661d0c295bea97
pasanpiyumantha98 <139437240+pasanpiyumantha98@users.noreply.github.com>
2025-06-19 12:53:03 +0530

    19june3

---
+++ arrays.java
@@ -0,0 +1,20 @@
+class Demo5{
+
+
+public static void main(String args[]){
+
+
+String names[]= new String[2];
+
+names[0]="Nimal";
+names[1]="Kasun";
+
+for(String x : names)
+{
+    System.out.println(x);
+}
+
+
+}
+
+}
